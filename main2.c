b5e89e025073fe1944b238f1f78f97e838658636
chaeyoung0813 <chaeyoung0813@naver.com>
2022-10-18 20:00:32 +0900

    6-2

--- main2.c
+++ main2.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/*
-int all_files;
 
+
+static int all_files;
 extern void sub();
-
 int main(void)
 {
-	sub();
-	printf("%d\n", all_files);
-	return 0;
+sub();
+printf(¡°%d\n¡±, all_files);
+return 0;
 }
-*/
+
+
 extern int all_files;
 void sub(void)
 {
