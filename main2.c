e452e60cff7ca7920c08d6290257410cb682aeff
chaeyoung0813 <chaeyoung0813@naver.com>
2022-10-18 19:47:47 +0900

    2

--- main2.c
+++ main2.c
@@ -5,13 +5,15 @@
 int main(void)
 {
 	int flag=1;
-	while(flag!=0)
+	int i;
+	
+	for (i=0; i<5; i++)
 	{
-		int y;
-		y=3;
-		flag=0;
-		y=4;
+		int temp=1;
+		printf("temp=%d\n", temp);
+		temp++;
 	}
+	return 0;
 }
 
 	
